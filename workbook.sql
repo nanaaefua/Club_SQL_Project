f73fb9244982af80a54b1ea442176915a8a60d8e
nanaaefua <equansah1@gmail.com>
2022-09-09 15:48:41 -0500

    progress

--- workbook.sql
+++ workbook.sql
@@ -3,51 +3,41 @@ USE mydb;
 /* ============================================================================ */
 /* SECTION 1: SELECT statements                                                 */
 /* ============================================================================ */
-
-
-
-
-
-
+-- SELECT * FROM us_state WHERE name = "California"
+-- SELECT name, capital FROM us_state WHERE name = "California"
+ 
 /* ---- TASK 1 ---------------------------------------------------------------- */
 /* Write an SQL statement to find out what is the state capital of Oregon       */
 /* hint: the names of the columns you need are: name and capital                */
 /* ---------------------------------------------------------------------------- */
-
-
-
-
-
-
+-- SELECT name, capital FROM us_state WHERE name = "Oregon"
+-- SELECT name, capital FROM us_state WHERE name = "Maryland"
+-- SELECT name, capital FROM us_state WHERE name = "Tennessee"
+-- SELECT * FROM us_state WHERE area_size > 10000
+-- SELECT * FROM us_state WHERE area_size <= 10000
 
 /* ============================================================================ */
 /* SECTION 2: SELECT statements with pattern matching and sorting               */
 /* ============================================================================ */
-
-
-
-
-
+-- SELECT * FROM us_state ORDER BY area_size 
+-- SELECT * FROM us_state ORDER BY area_size desc
+-- SELECT * FROM us_state WHERE name LIKE "M%"
+-- SELECT * FROM us_state WHERE name LIKE "M%" ORDER BY name
 
 /* ---- TASK 2 ---------------------------------------------------------------- */
 /* Write an SQL statement to display all the states with abbreviations that     */
 /* start with the letter "N", sort your results by state capital                */
 /* ---------------------------------------------------------------------------- */
 
-
-
-
-
-
+-- SELECT * FROM us_state WHERE abbreviation like "N%" ORDER BY name
+--
 /* ============================================================================ */
 /* SECTION 3: INSERT statements                                                 */
 /* ============================================================================ */
-
-
-
-
-
-
+-- INSERT INto club_member
+-- SET first_name = "Harrison",
+-- last_name = "Kong",
+-- phone_number = "3101234567"
 
 /* ---- TASK 3 ---------------------------------------------------------------- */
 /* Using the above example as a guide,                                          */
@@ -55,47 +45,36 @@ USE mydb;
 /* set first_name to "John", last_name to "Doe"                                 */
 /* and phone_number to "9998887777"                                             */
 /* ---------------------------------------------------------------------------- */
-
-
-
-
-
-
-
+-- INSERT INTO  club_member
+-- SET first_name = "John",
+-- last_name = "Doe",
+-- phone_number = "9998887777"
 
 /* ============================================================================ */
 /* SECTION 4: DELETE statements                                                 */
 /* ============================================================================ */
 
-
-
-
-
+-- DELETE FROM club_member
+-- WHERE first_name = "Harrison"
+-- AND last_name = "Kong"
 
 /* ---- TASK 4 ---------------------------------------------------------------- */
 /* Using the above example as a guide,                                          */
 /* write an SQL statement to delete the row in the table club_member            */
 /* if the phone_number is "9998887777"                                          */  
 /* ---------------------------------------------------------------------------- */
-
-
-
-
-
-
-
-
-
+-- DELETE FROM club_member
+ -- WHERE phone_number = "9998887777"
 
 /* ============================================================================ */
 /* SECTION 5: Joining tables                                                    */
 /* ============================================================================ */
 
-
-
-
-
-
+-- SELECT club_member.member_id, first_name,
+-- last_name, description, amount, due_date
+-- FROM club_member, invoice
+-- WHERE club_member.member_id = 
+-- invoice.member_id
 
 
 /* ---- TASK 5 ---------------------------------------------------------------- */
@@ -104,7 +83,9 @@ USE mydb;
 /*                                                                              */
 /* 1. include these columns in your results:                                    */
 /*    first_name, middle_name, last_name, professional_title, tier.description  */
-/*                                                                              */
+/*            
+
+                                                                  */
 /* 2. your columns will come from the tables club_member and tier               */
 /*                                                                              */
 /* 3. linking the tier_code column of the table club_member                     */
